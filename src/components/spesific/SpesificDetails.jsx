7723b0dd0876eaf7a44fb4a363c068581291d511
martineleirvik <leirvikmartine@gmail.com>
2022-09-09 14:08:28 +0200

    spesific

---
+++ src/components/spesific/SpesificDetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SpesificDetails({ id, name }) {
+  return (
+    <>
+      <h1>{name}</h1>
+    </>
+  );
+}
+
+export default SpesificDetails;
--- src/pages/Spesific.jsx
+++ src/pages/Spesific.jsx
@@ -1,5 +1,70 @@
+import { useState, useEffect } from "react";
+import { useParams, useNavigate, Navigate } from "react-router-dom";
+import { baseUrl } from "../constants/api";
+//components
+import SpesificDetails from "../components/spesific/SpesificDetails";
+import Loader from "../components/layout/Loader";
+import IfPopular from "../components/booking/IfPopular";
+//styles
+import styled from "styled-components";
+
 function Spesific() {
-  return <div>Spesific</div>;
+  const [bookingDetail, setBookingDetail] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  let Navigate = useNavigate();
+
+  const { id } = useParams();
+
+  if (!id) {
+    Navigate("/");
+  }
+
+  const url = baseUrl + "bookings/" + id;
+
+  useEffect(
+    function () {
+      async function fetchData() {
+        try {
+          const response = await fetch(url);
+          if (response.ok) {
+            const json = await response.json();
+            console.log(json.data);
+            setBookingDetail(json.data);
+          } else {
+            setError("An error occured");
+          }
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
+      }
+      fetchData();
+    },
+    [url]
+  );
+
+  if (loading) {
+    return <Loader />;
+  }
+  if (error) {
+    return <div>ERROR: {error}</div>;
+  }
+
+  return (
+    <div className="booking-detail">
+      {Object.entries(bookingDetail).map(function (detail) {
+        const { name } = detail.attributes;
+        return (
+          <>
+            <SpesificDetails name={name} />
+          </>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Spesific;
