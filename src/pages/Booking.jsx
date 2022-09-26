20885be3b996b050f4a5a20b5e1a3cbae42818c3
martineleirvik <leirvikmartine@gmail.com>
2022-09-26 08:49:20 +0200

    Filterbuttons

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -13,6 +13,7 @@ function Booking() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [filterBtn, setFilterBtn] = useState("bookings?populate=*");
+  const [currentActiveBtn, setCurrentActiveBtn] = useState("all");
 
   useEffect(
     function () {
@@ -56,36 +57,50 @@ function Booking() {
   return (
     <>
       <Heading heading="Booking" />
-      <StyledFilterBtns>
-        <button onClick={() => setFilterBtn("bookings?populate=*")}>All</button>
-        <button
-          onClick={() =>
+      <StyledFilterBtnsContainer>
+        <StyledFilterBtn
+          isActive={currentActiveBtn === "all"}
+          onClick={() => {
+            setFilterBtn("bookings?populate=*");
+            setCurrentActiveBtn("all");
+          }}
+        >
+          All
+        </StyledFilterBtn>
+        <StyledFilterBtn
+          isActive={currentActiveBtn === "hotels"}
+          onClick={() => {
             setFilterBtn(
               "categories/1?fields=name&populate[bookings][populate][0]=image"
-            )
-          }
+            );
+            setCurrentActiveBtn("hotels");
+          }}
         >
           Hotels
-        </button>
-        <button
-          onClick={() =>
+        </StyledFilterBtn>
+        <StyledFilterBtn
+          isActive={currentActiveBtn === "bnb"}
+          onClick={() => {
             setFilterBtn(
               "categories/2?fields=name&populate[bookings][populate][0]=image"
-            )
-          }
+            );
+            setCurrentActiveBtn("bnb");
+          }}
         >
           B&B's
-        </button>
-        <button
-          onClick={() =>
+        </StyledFilterBtn>
+        <StyledFilterBtn
+          isActive={currentActiveBtn === "guesthouses"}
+          onClick={() => {
             setFilterBtn(
               "categories/3?fields=name&populate[bookings][populate][0]=image"
-            )
-          }
+            );
+            setCurrentActiveBtn("guesthouses");
+          }}
         >
           Guesthouses
-        </button>
-      </StyledFilterBtns>
+        </StyledFilterBtn>
+      </StyledFilterBtnsContainer>
       <StyledBookingContainer>
         {bookings.map(function (booking) {
           const { id } = booking;
@@ -126,24 +141,30 @@ const StyledBookingContainer = styled.div`
   }
 `;
 
-const StyledFilterBtns = styled.div`
+const StyledFilterBtnsContainer = styled.div`
   margin: 15px 0 15px 50px;
-  button {
-    cursor: pointer;
-    margin: 5px 2px;
-    :hover {
-      background-color: ${(props) => props.theme.white};
-      border: 1px solid ${(props) => props.theme.secondaryColor};
-      color: ${(props) => props.theme.secondaryColor};
-    }
+
+  @media (max-width: 480px) {
+    margin: 10px;
+  }
+`;
+
+const StyledFilterBtn = styled.button`
+  cursor: pointer;
+  margin: 5px 2px;
+  background-color: ${(props) =>
+    props.isActive ? props.theme.white : props.theme.secondaryColor};
+  color: ${(props) =>
+    props.isActive ? props.theme.secondaryColor : props.theme.white};
+  :hover {
+    background-color: ${(props) => props.theme.white};
+    border: 1px solid ${(props) => props.theme.secondaryColor};
+    color: ${(props) => props.theme.secondaryColor};
   }
   .active {
     background-color: ${(props) => props.theme.white};
     border: 1px solid ${(props) => props.theme.secondaryColor};
     color: ${(props) => props.theme.secondaryColor};
-  }
-  @media (max-width: 480px) {
-    margin: 10px;
   }
 `;
 
