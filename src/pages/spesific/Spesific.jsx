e470574befa6f639d57beb222ff06e39c8bf66ea
martineleirvik <leirvikmartine@gmail.com>
2022-10-01 15:22:02 +0200

    small fixes

--- src/pages/spesific/Spesific.jsx
+++ src/pages/spesific/Spesific.jsx
@@ -38,7 +38,7 @@ function Spesific() {
             setError("An error occured");
           }
         } catch (error) {
-          console.log(error);
+          setError(error.toString());
         } finally {
           setLoading(false);
         }
